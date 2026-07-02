b7cafd4ee9df0cc1b2b5bcc416f2273e0afd158c
Thael <thaelzaruz@gmail.com>
2026-07-02 02:43:13 -0300

    insercao de usuario com post

--- src/main/java/com/ttech/workshopmongo/services/UserService.java
+++ src/main/java/com/ttech/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ttech.workshopmongo.domain.User;
+import com.ttech.workshopmongo.dto.UserDTO;
 import com.ttech.workshopmongo.repository.UserRepository;
 import com.ttech.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert (User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO (UserDTO objDTO) {
+		return new User (objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
