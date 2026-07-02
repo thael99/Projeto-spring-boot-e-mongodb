c4a5f0298f594c9597607f7d67a6683fa45c643a
Thael <thaelzaruz@gmail.com>
2026-07-02 01:37:05 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/ttech/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ttech/workshopmongo/config/Instantiation.java
@@ -22,7 +22,6 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	}
 
--- src/main/java/com/ttech/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ttech/workshopmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.ttech.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ttech.workshopmongo.domain.User;
+import com.ttech.workshopmongo.dto.UserDTO;
 import com.ttech.workshopmongo.services.UserService;
 
 @RestController
@@ -23,12 +23,13 @@ public class UserResource {
 	
 	//@RequestMapping (method = RequestMethod.GET)
 	@GetMapping
-	public ResponseEntity <List<User>> findAll(){
+	public ResponseEntity <List<UserDTO>> findAll(){
 		//User maria = new User("1", "Maria Silva", "maria@gmail.com");
 		//User alex = new User("2", "Alex Green", "alex@gmail.com");
 		//List<User> list = new ArrayList<>();
 		//list.addAll(Arrays.asList(maria, alex));
 		List<User> list =service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
