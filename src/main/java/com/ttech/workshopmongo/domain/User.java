73727f5ecae4e1233a68369613973f7d93a70ea0
Thael <thaelzaruz@gmail.com>
2026-07-03 02:03:39 -0300

    referenciando os posts do usuario

--- src/main/java/com/ttech/workshopmongo/domain/User.java
+++ src/main/java/com/ttech/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.ttech.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -13,6 +16,8 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	@DBRef (lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -47,6 +52,13 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 	
 	@Override
 	public int hashCode() {
@@ -64,5 +76,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 	
 }
